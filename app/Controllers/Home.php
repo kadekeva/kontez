6b47dead19d33d58e855ce27cfbc4754bd4c9ca3
dwikyls <kreuzvernicterlanze@gmail.com>
2020-09-06 13:21:57 +0700

    add: navbar & konten home

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,12 +1,18 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 class Home extends BaseController
 {
-	public function index()
-	{
-		return view('welcome_message');
-	}
+    public function index()
+    {
+        $data = [
+            'title' => 'Home | Kontez'
+        ];
 
-	//--------------------------------------------------------------------
+        return view('index', $data);
+    }
+
+    //--------------------------------------------------------------------
 
 }
---
+++ app/Views/layout/navbar.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">Kontez</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="#">Pricing</a>
+            </div>
+        </div>
+        <div class="dropdown">
+            <button class="btn btn-secondary" type="button" id="login" data-toggle="dropdown">
+                Sign in
+            </button>
+            <div class="dropdown-menu" aria-labelledby="login">
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Username" aria-label="Search">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Password" aria-label="Search">
+                    <button class="dropdown-item btn btn-primary my-2 my-sm-0" type="submit">Login</button>
+                </form>
+            </div>
+        </div>
+
+    </div>
+</nav>
