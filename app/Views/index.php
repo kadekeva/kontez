9ae077e0191463b84b4756a055304ad5ef098b27
dwikyls <kreuzvernicterlanze@gmail.com>
2020-09-06 14:02:00 +0700

    add: brand-logo footer, fix: navbar & index

--- app/Views/index.php
+++ app/Views/index.php
@@ -4,33 +4,46 @@
 
 <div class="container mt-3">
     <div class="row">
-        <div class="col-6 bg1">
-        </div>
+
+        <!-- TEKS TENTANG COMPANY -->
         <div class="col-6">
+
+        </div>
+
+        <!-- FORMULIR PENDAFTARAN -->
+        <div class="col-6 bg-light pt-5 pb-5 rounded">
             <form>
                 <div class="form-row">
+
+                    <!-- EMAIL & PASSWORD, BISA NAMA DEPAN DAN BELAKANG JUGA -->
                     <div class="form-group col-md-6">
                         <label for="inputEmail4">Email</label>
-                        <input type="email" class="form-control" id="inputEmail4">
+                        <input type="email" class="form-control" placeholder="email@example.com" id="inputEmail4">
                     </div>
+
                     <div class="form-group col-md-6">
                         <label for="inputPassword4">Password</label>
                         <input type="password" class="form-control" id="inputPassword4">
                     </div>
                 </div>
+
                 <div class="form-group">
                     <label for="inputAddress">Address</label>
                     <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
                 </div>
+
                 <div class="form-group">
                     <label for="inputAddress2">Address 2</label>
                     <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
                 </div>
                 <div class="form-row">
+
+                    <!-- ISI APALAH DISINI SEMBARANG -->
                     <div class="form-group col-md-6">
                         <label for="inputCity">City</label>
                         <input type="text" class="form-control" id="inputCity">
                     </div>
+
                     <div class="form-group col-md-4">
                         <label for="inputState">State</label>
                         <select id="inputState" class="form-control">
@@ -38,19 +51,24 @@
                             <option>...</option>
                         </select>
                     </div>
+
                     <div class="form-group col-md-2">
                         <label for="inputZip">Zip</label>
                         <input type="text" class="form-control" id="inputZip">
                     </div>
                 </div>
+
+                <!-- CAPTCHA & RULES RULES -->
                 <div class="form-group">
                     <div class="form-check">
                         <input class="form-check-input" type="checkbox" id="gridCheck">
                         <label class="form-check-label" for="gridCheck">
-                            Check me out
+                            Saya menyetujui persyaratan dan peraturan yang berlaku
                         </label>
                     </div>
                 </div>
+
+                <!-- TOMBOL SUBMIT -->
                 <button type="submit" class="btn btn-primary">Sign up</button>
             </form>
         </div>
