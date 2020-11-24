c5833368a9f6f2717d6eaf558d13730829ee0e64
dwikyls <kreuzvernicterlanze@gmail.com>
2020-11-24 19:02:39 +0700

    nyoba push

--- app/Views/index.php
+++ app/Views/index.php
@@ -3,69 +3,7 @@
 <?= $this->section('content'); ?>
 
 <div class="container mt-5">
-    <div class="row justify-content-end">
-        <!-- FORMULIR PENDAFTARAN -->
-        <div class="bg-light p-4 m-3 rounded">
-            <form action="" method="">
-                <div class="form-row">
-                    <!-- EMAIL & PASSWORD, BISA NAMA DEPAN DAN BELAKANG JUGA -->
-                    <div class="form-group col-md-6">
-                        <label for="inputEmail4">Email</label>
-                        <input type="email" class="form-control" placeholder="email@example.com" id="inputEmail4">
-                    </div>
 
-                    <div class="form-group col-md-6">
-                        <label for="inputPassword4">Password</label>
-                        <input type="password" class="form-control" id="inputPassword4">
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label for="inputAddress">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="Address">
-                </div>
-
-                <div class="form-group">
-                    <label for="inputAddress2">Address 2</label>
-                    <input type="text" class="form-control" id="inputAddress2" placeholder="Address ">
-                </div>
-                <div class="form-row">
-
-                    <!-- ISI APALAH DISINI SEMBARANG -->
-                    <div class="form-group col-md-6">
-                        <label for="inputCity">City</label>
-                        <input type="text" class="form-control" id="inputCity">
-                    </div>
-
-                    <div class="form-group col-md-4">
-                        <label for="inputState">State</label>
-                        <select id="inputState" class="form-control">
-                            <option selected>Choose...</option>
-                            <option>...</option>
-                        </select>
-                    </div>
-
-                    <div class="form-group col-md-2">
-                        <label for="inputZip">Zip</label>
-                        <input type="text" class="form-control" id="inputZip">
-                    </div>
-                </div>
-
-                <!-- CAPTCHA & RULES RULES -->
-                <div class="form-group">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="gridCheck">
-                        <label class="form-check-label" for="gridCheck">
-                            Saya menyetujui persyaratan dan peraturan yang berlaku
-                        </label>
-                    </div>
-                </div>
-
-                <!-- TOMBOL SUBMIT -->
-                <button type="submit" class="btn btn-primary">Sign up</button>
-            </form>
-        </div>
-    </div>
 </div>
 
 <div class="container mt-5">
