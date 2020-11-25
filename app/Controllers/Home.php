e96db3e0d5e47d741306ecc301574e955b7345b4
dwikyls <kreuzvernicterlanze@gmail.com>
2020-11-25 13:43:12 +0700

    add: login route to controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -13,6 +13,24 @@ class Home extends BaseController
         return view('index', $data);
     }
 
+    public function login()
+    {
+        $data = [
+            'title' => 'Login'
+        ];
+
+        return view('auth/login', $data);
+    }
+
+    public function regis()
+    {
+        $data = [
+            'title' => 'Registrasi'
+        ];
+
+        return view('auth/regis', $data);
+    }
+
     //--------------------------------------------------------------------
 
 }
