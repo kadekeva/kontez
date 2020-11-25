81662e9c4ee5f9880d64657725c785ff459b90b8
dwikyls <kreuzvernicterlanze@gmail.com>
2020-11-25 13:42:49 +0700

    add: login page

---
+++ app/Views/auth/login.php
@@ -0,0 +1,11 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+
+<div class="container">
+    <div class="login">
+
+    </div>
+</div>
+
+<?= $this->endSection(); ?>
