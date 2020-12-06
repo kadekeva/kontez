f9317d4c8dec452b21d1923b332ee58ff06094a4
Ni Kadek Eva Anggreni <48253616+kadekeva@users.noreply.github.com>
2020-12-06 22:45:59 +0700

    membuat file detail lomba.php
    berisi fitur - fitur detail lomba dan harap koreksi

---
+++ app/Views/user/detaillomba.php
@@ -0,0 +1,59 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container mr-5">
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">
+    <img src="brand-1.jpg" width="30" height="30">
+  </a>
+  <span class="fa-user-circle">
+  <i class="fa fa-user-circle">Hello Andi Budi</i>
+</span>
+</nav>
+<a class="btn btn-primary href="#" role="button">Kembali</a>
+<div class="col-md-6">
+<h1> Detail Lomba </h1>
+<img src="poster.png" width="30" height="30">
+<div class="container">
+<div class="row">
+    <div class="col-md-8">
+    <h1> Lomba Desain Poster Digital </h1>
+    </div>
+    <div class="col-sm-8">
+      <h2>Deskripsi Acara</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+          et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+          ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+          esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+          sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h2> Syarat </h2>
+      <p> Minimal SMA/K Sederajat  dan mempunyai kemampuan desain.</p>
+      <h2>Benefit</h2>
+      <ul>
+        <li> Sertifikat </li>
+        <li> Pengalaman Baru </li>
+      </ul>
+      <h2>Hadiah</h2>
+      <p>Uang Tunai</p>
+      </div>
+    <div class="col-sm-4">
+     <h3> Deadline </h3>
+     <p> 3 Hari 4 jam Lagi </p>
+     <h3> Sisa Peserta </h3>
+     <p> 100 Peserta </p>
+     <h3> Jadwal </h3>
+     <p> 27 November Pukul 20.00 </p>
+     <h3> Lokasi </h3>
+     <p> Etiopia Selatan no 4 </p>
+     <h3> Biaya </h3>
+     <p> Rp. 0 </p>
+
+     <button type="button" class="btn btn-primary btn-lg btn-block">Anda Terdaftar</button>
+    <button type="button" class="btn btn-secondary btn-lg btn-block">Setel Pengingat</button>
+    </div>
+</div>
+
+  </div>
+</div>
+</div>
+</div>
